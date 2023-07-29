a599634055c026c20c5fd8b56bf91afe746350bc
Guilherme <guilhermecourbassier122@gmail.com>
2023-07-29 06:35:19 -0300

    <feat>: Adding coverage test on project

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/tests/**/*.test.ts'],
+    collectCoverage: true,
+    coverageReporters: ["lcov", "text"],
 };
   
